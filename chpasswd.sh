ebec72ac1ccab0a84cb46d388afca9bff0489e98
wuyun6 <wuyun19660606@gmail.com>
2024-06-01 01:44:21 +0800

    Updated tip

--- chpasswd.sh
+++ chpasswd.sh
@@ -4,6 +4,7 @@ while true; do
     read -p "请输入新的root密码: " rootpw
     read -p "请再次输入新的root密码: " rootpw_verify
     if [ "$rootpw" = "$rootpw_verify" ]; then
+        echo "输入的密码为: [$rootpw]"
         break
     else
         echo "两次输入的密码不一致，请重新输入."
